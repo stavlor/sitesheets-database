866f6deef2a82cfdca9f4050fbf280ab89a9df52
Paul Andrel <stavlor@stavlor.net>
2015-09-09 14:06:41 -0400

    Begining work on mw page processor

--- process-page.py
+++ process-page.py
@@ -3,7 +3,7 @@ import sys
 from pymongo import MongoClient
 
 useragent = "MW Database update bot 0.1, Run by Paul.Andrel@kantarmedia.com"
-site - mwclient.Site('172.18.64.38', '/index.php')
+site = mwclient.Site('172.18.64.38', '/index.php')
 
 site.login('pandrel', '4357pja1')
 
@@ -12,4 +12,12 @@ pages = []
 #/ Process commandline arguments into pages
 for arg in sys.argv:
     pages.append(arg)
+
+def processPageText(text, page):
+    noop
+
+for page in pages:
+    mpage = site.Pages(page)
+    text = mpage.text()
+    processPageText(text, page)
     
