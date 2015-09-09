266495e598a1ec1889ca84d9ebb2bab36ff70f67
Paul Andrel <stavlor@stavlor.net>
2015-09-09 16:42:27 -0400

    Mostly done need to pull out comments before trying to output keys

--- process-page.py
+++ process-page.py
@@ -1,23 +1,32 @@
 import mwclient
+import mwparserfromhell
 import sys
 from pymongo import MongoClient
 
 useragent = "MW Database update bot 0.1, Run by Paul.Andrel@kantarmedia.com"
-site = mwclient.Site('172.18.64.38', '/index.php')
+site = mwclient.Site('172.18.64.38', '/')
 
-site.login('pandrel', '4357pja1')
+site.login('pandrel', '4357pja')
 
 #/ Set up an empty array to capture page list
-pages = []
+pages = ['Sitesheets:PAPH2']
 #/ Process commandline arguments into pages
-for arg in sys.argv:
-    pages.append(arg)
+#for arg in sys.argv:
+#    pages.append(arg)
+#    print arg
 
 def processPageText(text, page):
-    noop
+    wikicode = mwparserfromhell.parse(text)
+    templates = wikicode.filter_templates()
+    template = templates[0]
+    for item in template.params:
+        if item.startswith(u"<!-- "):
+            continue
+        item = item.rstrip('\n')
+        print item
 
 for page in pages:
-    mpage = site.Pages(page)
+    mpage = site.Pages[page]
     text = mpage.text()
     processPageText(text, page)
     
