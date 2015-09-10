11bfd2fab19edf6463da6e8e2b4ff81d7297e958
Paul Andrel <stavlor@stavlor.net>
2015-09-10 11:47:29 -0400

    Continued work to get data keys/values and  produce json

--- process-page.py
+++ process-page.py
@@ -1,6 +1,8 @@
 import mwclient
 import mwparserfromhell
 import sys
+import json
+from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
 useragent = "MW Database update bot 0.1, Run by Paul.Andrel@kantarmedia.com"
@@ -9,21 +11,31 @@ site = mwclient.Site('172.18.64.38', '/')
 site.login('pandrel', '4357pja')
 
 #/ Set up an empty array to capture page list
-pages = ['Sitesheets:PAPH2']
+pages = ['Sitesheets:AKFA1']
 #/ Process commandline arguments into pages
 #for arg in sys.argv:
 #    pages.append(arg)
 #    print arg
 
 def processPageText(text, page):
+    data = {}
     wikicode = mwparserfromhell.parse(text)
     templates = wikicode.filter_templates()
     template = templates[0]
     for item in template.params:
-        if item.startswith(u"<!-- "):
-            continue
-        item = item.rstrip('\n')
-        print item
+        item = str(item)
+        soup = BeautifulSoup(item, "lxml")
+        ckey = item.split("=", 1)
+        if ckey[0] != 'comments':
+            item = soup.text
+            key, value= item.split("=",1)
+            value = value.rstrip('\n')
+        else:
+            key, value = item.split("=",1)
+            value = value.rstrip('\n')
+        data[key.strip()] = value.strip()
+    print json.dumps(data)
+    print data[u"comments"]
 
 for page in pages:
     mpage = site.Pages[page]
